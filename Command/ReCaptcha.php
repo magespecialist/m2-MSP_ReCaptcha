65e6e92475fac3d1776a96a5f27f3d088cd7cac8
Riccardo Tempesta <info@riccardotempesta.com>
2017-09-22 16:45:18 +0200

    FIX wrong config key

--- Command/ReCaptcha.php
+++ Command/ReCaptcha.php
@@ -22,6 +22,7 @@ namespace MSP\ReCaptcha\Command;
 
 use Magento\Framework\App\Cache\Manager;
 use Magento\Framework\App\Config\ConfigResource\ConfigInterface;
+use MSP\ReCaptcha\Model\Config;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -58,7 +59,7 @@ class ReCaptcha extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->config->saveConfig(
-            Data::XML_PATH_GENERAL_ENABLED_BACKEND,
+            Config::XML_PATH_ENABLED_BACKEND,
             '0',
             'default',
             0
