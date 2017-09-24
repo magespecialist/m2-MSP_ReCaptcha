91f8fb4a0d1f1e190535ad4c90bfe070e01541b2
Riccardo Tempesta <info@riccardotempesta.com>
2017-09-24 15:24:34 +0200

    JSCS fix

--- view/frontend/web/js/ui-messages-mixin.js
+++ view/frontend/web/js/ui-messages-mixin.js
@@ -19,33 +19,33 @@
 
 define(['MSP_ReCaptcha/js/registry'], function (registry) {
 
-    return function (originalComponent) {
-        return originalComponent.extend({
-            /**
-             * Initialize reset on messages
-             * @returns {initialize}
-             */
-            initialize: function () {
-                this._super();
+return function (originalComponent) {
+    return originalComponent.extend({
+        /**
+         * Initialize reset on messages
+         * @returns {initialize}
+         */
+        initialize: function () {
+            this._super();
 
-                this.messageContainer.errorMessages.subscribe(function () {
-                    var
-                        i,
-                        captchaList = registry.captchaList(),
-                        tokenFieldsList = registry.tokenFields();
+            this.messageContainer.errorMessages.subscribe(function () {
+                var
+                    i,
+                    captchaList = registry.captchaList(),
+                    tokenFieldsList = registry.tokenFields();
 
-                    for (i = 0; i < captchaList.length; i++) {
-                        // eslint-disable-next-line no-undef
-                        grecaptcha.reset(captchaList[i]);
+                for (i = 0; i < captchaList.length; i++) {
+                    // eslint-disable-next-line no-undef
+                    grecaptcha.reset(captchaList[i]);
 
-                        if (tokenFieldsList[i]) {
-                            tokenFieldsList[i].value = '';
-                        }
+                    if (tokenFieldsList[i]) {
+                        tokenFieldsList[i].value = '';
                     }
-                }, null, 'arrayChange');
+                }
+            }, null, 'arrayChange');
 
-                return this;
-            }
-        });
-    };
+            return this;
+        }
+    });
+};
 });
